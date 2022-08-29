9014159346751ac17d152976897bf1be3c35c532
William <william.hardy.abeloos@gmail.com>
2022-08-29 15:37:05 -0400

    finish the project

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
-let computerChoice = Math.random()
-let userScore = 0
-let computerScore = 0
-if (computerChoice <0.34) {
-    computerChoice='rock';
-}    
-    else if (computerChoice<=0.67){
-        computerChoice='paper';
-    }
-    else {
-        computerChoice='scissors'
-    }
 
-let userChoice=prompt('Do you choose Rock, Paper, or Scissor?').toLowerCase()
+    let userScore = 0
+    let computerScore = 0
 
-
-function playRound (x, y) { /*x is user and y is computer*/
-    if (x==y) {
+function playRound () { /*x is user and y is computer*/
+    let userChoice=prompt('Do you choose Rock, Paper, or Scissor?').toLowerCase()
+    let computerChoice = Math.random()
+    if (computerChoice <0.34) {
+        computerChoice='rock';
+    }    
+        else if (computerChoice<=0.67){
+            computerChoice='paper';
+        }
+        else {
+            computerChoice='scissors'
+        }
+    if (userChoice==computerChoice) {
         return 'The result is a tie!';
     }
-    else if (x=='rock') {
-        if (y=='scissors') {
+    else if (userChoice=='rock') {
+        if (computerChoice=='scissors') {
             userScore++;   /*same as userScore += 1 or userScore = userScore +1? */
+            console.log('User' + userScore)
             return 'You Win';
         }
         else {
             computerScore++;
+            console.log('Comp' + computerScore)
             return 'You Lose';
         }
     }
-    else if (x=='paper') {
-        if (y=='scissors') {
+    else if (userChoice=='paper') {
+        if (computerChoice=='scissors') {
             computerScore++;
+            console.log('Com' + computerScore)
             return 'You Lose';
         }
         else {
             userScore++;
+            console.log('User' + userScore)
             return 'You Win';
         }
     }
-    else if (x=='scissors') {
-        if (y=='paper') {
+    else if (userChoice=='scissors') {
+        if (computerChoice=='paper') {
             userScore++;
+            console.log('User' + userScore)
             return 'You Win';
         }
         else {
             computerScore++;
+            console.log('Comp' + computerScore)
             return 'You Lose';
         }
     }
 }
-console.log(computerChoice)
-console.log(userChoice)
-console.log(playRound (userChoice, computerChoice))
+//console.log(computerChoice)
+//console.log(playRound (computerChoice))
 
 
 // r is userScore and t is computerScore
 // do I need to add userChoice and computerChoice in parameters?
-function game (r, t) {
+function game () {
     for (let i = 1; i <= 5; i++) {
-        playRound(userChoice, computerChoice)
+        playRound()
     }
-    if (r > t) {
+    if (userScore == computerScore) {
+        console.log(userScore);
+        console.log(computerScore);
+        return 'It is a big tie!'
+    }
+    else if (userScore > computerScore) {
+        console.log(userScore);
+        console.log(computerScore);
         return 'You are the grand Winner!'
     }
     else {
+        console.log(userScore);
+        console.log(computerScore);
         return 'You are a loser!'
     }
 }
@@ -73,3 +86,6 @@ consoler.log (computerScore)
 */
 
 //diagrams
+
+//always saying tie and also I get all the points for rock
+// add option if person enters nothing
